8979e61dc1ba7e441b99c913e0d2f85a0eee8719
Robert Drescher <robert@macombhometeam.com>
2022-12-22 14:24:16 -0500

    Add errorhandler and errorlog

---
+++ errorhandling/errorhandler.py
@@ -0,0 +1,125 @@
+import logging
+import traceback
+
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+from .errorlog import ErrorLog
+
+_logger = logging.getLogger(__name__)
+
+class ErrorHandler(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot = bot
+
+    def cog_load(self) -> None:
+        tree = self.bot.tree
+        tree.on_error = self.on_app_command_error
+
+    def cog_unload(self) -> None:
+        tree = self.bot.tree
+        tree.on_error = tree.__class__.on_error
+
+    async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
+
+        if isinstance(error, app_commands.CommandOnCooldown):
+            if interaction.response.is_done():
+                await interaction.followup.send(f"A little too quick there, try again in {error.retry_after:,.1f} seconds.")
+            else:
+                await interaction.response.send_message(f"A little too quick there, try again in {error.retry_after:,.1f} seconds.")
+
+        else:
+            trace = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            errorlog = await ErrorLog.create(traceback=trace, item=f"Command: {interaction.command.name}")
+
+            _logger.error("Ignoring exception in app command {}:".format(interaction.command))
+            _logger.error(trace)
+
+            try:
+                if interaction.response.is_done():
+                    msg = await interaction.followup.send(embed=errorlog.pub_embed, wait=True)
+                    _logger.error(f"Notification message succesfully sent in {interaction.channel.id=} {msg.id=}")
+                else:
+                    # Interaction not responded.
+                    await interaction.response.send_message(embed=errorlog.pub_embed)
+                    _logger.error(f"Error notification message successfully sent as a response.")
+            except (discord.Forbidden, discord.HTTPException):
+                _logger.error(f"Could not send error notification in {interaction.channel.id=}")
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx: commands.Context, error: commands.CommandError) -> None:
+
+        # testing leaving this off
+        # if hasattr(ctx.command, 'on_error'):
+        #     return
+
+        cog = ctx.cog
+        if cog:
+            if cog._get_overridden_method(cog.cog_command_error) is not None:
+                return
+
+        ignored = (commands.CommandNotFound, )
+
+        error = getattr(error, 'original', error)
+
+        if isinstance(error, ignored):
+            return
+
+        if isinstance(error, commands.DisabledCommand):
+            await ctx.send(f'{ctx.command} has been disabled.', ephemeral=True)
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            try:
+                await ctx.author.send(f'{ctx.command} can not be used in Private Messages.', ephemeral=True)
+            except discord.HTTPException:
+                pass
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send(f"A little too quick there. Try again in {error.retry_after:,.1f} seconds.", delete_after=4.0, ephemeral=True)
+
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send(f"You do not have permission to use that command.", ephemeral=True)
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"You must have missed an argument ({error.param}), please try again.", ephemeral=True, delete_after=30.0)
+
+        elif isinstance(error, commands.BotMissingPermissions):
+            try:
+                perm_strings = [perm.replace("_", " ").title() for perm in error.missing_permissions]
+                await ctx.send(f"I am missing the permissions needed to run that command: {', '.join(perm_strings)}")
+            except discord.Forbidden:
+                _logger.info(f"Missing permissions {', '.join(perm_strings)} to run command '{ctx.command.qualified_name}' in channel_id={ctx.channel.id}")
+
+        elif isinstance(error, commands.UserNotFound):
+            await ctx.send(f"Could not find user {error.argument}.")
+
+        elif isinstance(error, commands.BadArgument):
+            await ctx.send(f"You provided an invalid argument.")
+
+        elif isinstance(error, commands.BadLiteralArgument):
+            await ctx.send(f"Invalid literal given, valid options: {' '.join(error.literals)}")
+
+        elif isinstance(error, commands.RangeError):
+            await ctx.send(f"Invalid value (**{error.value}**) given.")
+
+        else:
+            trace = "".join(traceback.format_exception(type(error), error, error.__traceback__))
+            errorlog = await ErrorLog.create(traceback=trace, item=f"Command: {ctx.command.name}")
+
+            _logger.error("Ignoring exception in command {}:".format(ctx.command))
+            _logger.error(trace)
+
+            try:
+                msg = await ctx.channel.send(embed=errorlog.pub_embed)
+                _logger.error(f"Notification message succesfully sent in {ctx.channel.id=} {msg.id=}")
+            except (discord.Forbidden, discord.HTTPException):
+                _logger.error(f"Could not send error notification in {ctx.channel.id=}")
+
+
+async def setup(bot):
+    _logger.info("Loading cog ErrorHandler")
+    await bot.add_cog(ErrorHandler(bot))
+
+async def teardown(bot):
+    _logger.info("Unloading cog ErrorHandler")
---
+++ errorhandling/errorlog.py
@@ -0,0 +1,169 @@
+
+from dataclasses import dataclass
+import datetime
+import io
+import logging
+
+import asqlite
+import discord
+from discord.ext import commands
+from typing_extensions import Self
+
+_logger = logging.getLogger(__name__)
+
+DB_FILENAME = "errorlog.sqlite"
+
+ERRORLOG_SETUP_SQL = """
+CREATE TABLE IF NOT EXISTS errorlog (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    unixtimestamp BIGINT NOT NULL,
+    traceback TEXT NOT NULL,
+    item TEXT NOT NULL
+)
+"""
+
+@dataclass(slots=True)
+class ErrorLog:
+    id: int
+    unixtimestamp: int
+    traceback: str
+    item: str
+
+    @classmethod
+    async def get_or_none(cls, id: int, /) -> Self | None:
+        async with asqlite.connect(DB_FILENAME) as db:
+            async with db.cursor() as cur:
+                await cur.execute("SELECT * FROM errorlog WHERE id = ?", id)
+
+                res = await cur.fetchone()
+
+                return cls(**res) if res is not None else None
+
+    @classmethod
+    async def create(cls, *, traceback: str, item: str) -> Self:
+        async with asqlite.connect(DB_FILENAME) as db:
+            async with db.cursor() as cur:
+                now_utc = int(discord.utils.utcnow().timestamp())
+                await cur.execute("INSERT INTO errorlog (unixtimestamp, traceback, item) VALUES (?, ?, ?) RETURNING *", now_utc, traceback, item)
+                res = await cur.fetchone()
+                await db.commit()
+
+                return cls(**res)
+
+    @classmethod
+    async def delete(cls, id: int, /) -> int:
+        async with asqlite.connect(DB_FILENAME) as db:
+            async with db.cursor() as cur:
+                await cur.execute("DELETE FROM errorlog WHERE id = ?", id)
+                await db.commit()
+
+                return cur.get_cursor().rowcount
+
+    @classmethod
+    async def get_most_recent(cls, num_to_get: int, /) -> list[Self] | None:
+        async with asqlite.connect(DB_FILENAME) as db:
+            async with db.cursor() as cur:
+                await cur.execute("SELECT * FROM errorlog ORDER BY id DESC LIMIT ?", num_to_get)
+
+                logs = await cur.fetchall()
+
+                return [cls(**res) for res in logs] if logs else None
+
+    @property
+    def timestamp(self) -> datetime.datetime:
+        """Returns a UTC datetime representing time of deletion"""
+        return datetime.datetime.fromtimestamp(self.unixtimestamp, tz=datetime.timezone.utc)
+
+    @property
+    def embed(self) -> discord.Embed:
+        """Generates an Embed that represents this Error.
+
+        Returns
+        -------
+        discord.Embed
+            The generated Embed.
+        """
+        embed = discord.Embed(title=f"Error #{self.id}{f' (Item/Command: {self.item})' if self.item is not None else ''}", color=discord.Color.blue())
+        embed.description = f"```{self.traceback[:5500]}```"
+        embed.set_footer(text=f"Occurred On: {self.timestamp:%m-%d-%Y} at {self.timestamp:%I:%M:%M %p} UTC")
+
+        return embed
+
+    @property
+    def pub_embed(self) -> discord.Embed:
+        """Returns the public embed message for this error.
+
+        Returns
+        -------
+        discord.Embed
+            The generated Embed.
+        """
+        return discord.Embed(title=f"An unexpected error occured (ID: {self.id})", description=f"My developers are aware of the issue.\n\nIf you want to discuss this error with my developer, join the [support server](https://discord.gg/f64pfnqbJJ \"Support Server Invite URL\") and refer to the error by it's id. (ID: {self.id})", color=discord.Color.blue())
+
+    @property
+    def raw_text(self) -> str:
+        """Returns the error in a raw text buffer."""
+        output = (
+            f"Error #{self.id}{f' (Item/Command: {self.item})' if self.item is not None else ''}\n"
+            f"Occurred On: {self.timestamp:%m-%d-%Y} at {self.timestamp:%I:%M:%M %p} UTC\n"
+            f"{self.traceback}\n"
+        )
+        return output
+
+    @property
+    def raw_bytes(self) -> io.BytesIO:
+        """Returns the error as raw UTF-8 encoded bytes buffer."""
+        output = io.BytesIO(self.raw_text.encode("UTF-8"))
+        output.seek(0)
+
+        return output
+
+
+class ErrorLogCog(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    async def cog_load(self) -> None:
+        async with asqlite.connect(DB_FILENAME) as db:
+            await db.execute(ERRORLOG_SETUP_SQL)
+
+    @commands.command(aliases=('e', ))
+    @commands.is_owner()
+    async def error(self, ctx: commands.Context, error_id: int, raw: bool = False) -> None:
+        """Sends an error from the database.
+
+        Parameters
+        ----------
+        error_id : int
+            The error id to send.
+        raw : bool
+            Whether to send the error in raw form, defaults to False
+        """
+        err = await ErrorLog.get_or_none(error_id)
+        if not err:
+            await ctx.send(f"I could not find an error with that id. (ID: {error_id})")
+            return
+        if raw:
+            await ctx.send(file=discord.File(err.raw_bytes, filename=f"{err.id} raw.txt"))
+        else:
+            await ctx.send(embed=err.embed)
+
+    @commands.command(aliases=('re',))
+    @commands.is_owner()
+    async def recenterrors(self, ctx: commands.Context) -> None:
+        """Returns the 20 most recently logged errors."""
+        errs = await ErrorLog.get_most_recent(20)
+        embed = discord.Embed(color=discord.Color.blue(), description="")
+        if errs:
+            for err in errs:
+                embed.description += f"{err.id:0>5}: (Item: {err.item}) {err.timestamp:%m-%d-%Y} at {err.timestamp:%I:%M:%M %p} UTC\n\n"
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send("No errors logged yet.")
+
+async def setup(bot: commands.Bot):
+    _logger.info("Loading cog ErrorLogCog")
+    await bot.add_cog(ErrorLogCog(bot))
+
+async def teardown(_: commands.Bot):
+    _logger.info("Unloading cog ErrorLogCog")
