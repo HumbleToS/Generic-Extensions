a03bea87b73e43b9a5b4f8e0a2b51cf7423aa4a0
Robert Drescher <robert@macombhometeam.com>
2022-12-31 18:31:20 -0500

    Finish tags

--- utility/tags.py
+++ utility/tags.py
@@ -5,23 +5,25 @@ from dataclasses import dataclass
 import logging
 
 import asqlite
+import discord
 from discord.ext import commands
 
 DB_FILENAME = "tags.sqlite"
 
 TAGS_SETUP_SQL = """
 CREATE TABLE IF NOT EXISTS tags (
-    id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
     name TEXT NOT NULL,
     owner_id BIGINT NOT NULL,
     guild_id BIGINT NOT NULL,
-    content TEXT NOT NULL
+    content TEXT NOT NULL,
+    PRIMARY KEY(guild_id, name)
 )
 """
 
+_logger = logging.getLogger(__name__)
+
 @dataclass(slots=True)
 class TagEntry:
-    id: int
     name: str
     owner_id: int
     guild_id: int
@@ -49,50 +51,144 @@ class TagEntry:
 
                 return cls(**res) if res is not None else None
 
-    @staticmethod
-    async def delete(*, name: str, guild_id: int) -> int:
+    async def delete(self) -> int:
         async with asqlite.connect(DB_FILENAME) as db:
             async with db.cursor() as cur:
-                await cur.execute("DELETE FROM tags WHERE name = ? AND guild_id = ?", name, guild_id)
+                await cur.execute("DELETE FROM tags WHERE name = ? AND guild_id = ?", self.name, self.guild_id)
                 await db.commit()
 
                 return cur.get_cursor().rowcount
 
-    async def update(self, *, content: str) -> TagEntry:
+    async def update(self, *, new_content: str = None) -> TagEntry:
+        to_update_to = new_content or self.content
         async with asqlite.connect(DB_FILENAME) as db:
             async with db.cursor() as cur:
-                await cur.execute("UPDATE tags SET content = ? WHERE name = ? AND guild_id = ? RETURNING *", content, self.name, self.guild_id)
+                await cur.execute("UPDATE tags SET content = ? WHERE name = ? AND guild_id = ? RETURNING *", to_update_to, self.name, self.guild_id)
                 await db.commit()
 
                 res = await cur.fetchone()
 
                 return TagEntry(**res)
 
-_logger = logging.getLogger(__name__)
 
 class TagsCog(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.group(invoke_without_subcommand=True)
-    async def tag(self, ctx: commands.Context, *, query: str) -> None:
+    async def cog_load(self) -> None:
+        async with asqlite.connect(DB_FILENAME) as db:
+            await db.execute(TAGS_SETUP_SQL)
+
+    @commands.group()
+    async def tag(self, ctx: commands.Context):
         pass
 
     @tag.command()
-    async def make(self, ctx: commands.Context, name: str, *, content: str) -> None:
-        pass
+    async def get(self, ctx: commands.Context, *, name: str) -> None:
+        """Gets a tag with given name
+
+        Parameters
+        ----------
+        name : str
+            The name of the tag to get.
+        """
+        tag = await TagEntry.get_or_none(name=name, guild_id=ctx.guild.id)
+
+        if tag is not None:
+            await ctx.send(tag.content)
+        else:
+            await ctx.send(f"Could not find tag with name `{name}`.")
+
+    @tag.command()
+    async def create(self, ctx: commands.Context, name: str, *, content: str) -> None:
+        """Creates a tag with given name and content.
+
+        Parameters
+        ----------
+        name : str
+            The name of the tag to create.
+        content : str
+            The content of the tag to create.
+        """
+        tag = await TagEntry.create(name=name, owner_id=ctx.author.id, guild_id=ctx.guild.id, content=content)
+
+        if tag is not None:
+            await ctx.send(f"Tag with name `{name}` successfully created.")
+        else:
+            await ctx.send(f"Tag with name `{name}` already exists.")
 
     @tag.command()
     async def delete(self, ctx: commands.Context, *, name: str) -> None:
-        pass
+        """Deletes a tag with given name.
+
+        Parameters
+        ----------
+        name : str
+            The name of the tag to delete
+        """
+        original = await TagEntry.get_or_none(name=name, guild_id=ctx.guild.id)
+        if not original:
+            await ctx.send(f"Tag with name `{name}` not found.")
+            return
+
+        if original.owner_id != ctx.author.id: # TODO MAKE THIS WORK FOR PEOPLE WITH ADMIN PERMS TOO
+            await ctx.send(f"You do not own the tag named `{name}`.")
+            return
+
+        removed = await original.delete()
+        if removed:
+            await ctx.send(f"Tag `{name}` deleted.")
+        else:
+            await ctx.send(f"Something went wrong while deleting `{name}`.")
 
     @tag.command()
     async def update(self, ctx: commands.Context, name: str, *, new_content: str) -> None:
-        pass
+        """Updates a tag with given name and new content.
+
+        Parameters
+        ----------
+        name : str
+            The name of the tag to update.
+        new_content : str
+            The new content for the tag.
+        """
+        original = await TagEntry.get_or_none(name=name, guild_id=ctx.guild.id)
+        if not original:
+            await ctx.send(f"Tag with name `{name}` not found.")
+            return
+
+        if original.owner_id != ctx.author.id:
+            await ctx.send(f"You do not own the tag named `{name}`.")
+            return
+
+        original.content = new_content
+        updated = await original.update()
+
+        await ctx.send(f"Tag with name {updated.name} content updated.")
 
     @tag.command()
     async def search(self, ctx: commands.Context, *, query: str) -> None:
-        pass
+        """Searches the tag list for tags with given query.
+
+        Parameters
+        ----------
+        query : str
+            The query to search for.
+        """
+        async with asqlite.connect(DB_FILENAME) as db:
+            async with db.cursor() as cur:
+                await cur.execute("SELECT name FROM tags WHERE name LIKE ? and guild_id = ?", f"%{query}%", ctx.guild.id)
+
+                results = await cur.fetchall()
+
+                if results:
+                    out = "\n".join(res['name'] for res in results[:20])
+                    if (num_results := len(results)) > 20:
+                        out += f"\n{num_results-20:,} other results."
+                    embed = discord.Embed(color=discord.Color.blue(), description=out, title=query)
+                    await ctx.send(embed=embed)
+                else:
+                    await ctx.send(f"No results found for `{query}`")
 
 async def setup(bot: commands.Bot):
     _logger.info("Loading cog TagsCog")
