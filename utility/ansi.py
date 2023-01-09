0672c4bd49bb17fe2ef894c6f2948af936491e86
Robert Drescher <robert@macombhometeam.com>
2023-01-08 20:40:31 -0500

    Update licenses

--- utility/ansi.py
+++ utility/ansi.py
@@ -3,6 +3,28 @@ This code created by combining multiple different items from other projects.
 
 Licenses for these portions of code are included above their respective parts, but
 may be slightly altered to ensure they work with the current discord.py version.
+
+All code not covered by one of the licenses contained within is licensed under the following:
+
+Copyright 2022-present fretgfr
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
 import collections
 import logging
